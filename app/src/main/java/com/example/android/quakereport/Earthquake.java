2fa4cf7b6c74ba238b4ae52078e8ad7349e4d1d4
OnkarVO7 <onkar.10r@gmail.com>
2018-05-31 19:49:30 +0530

    Initial commit

---
+++ app/src/main/java/com/example/android/quakereport/Earthquake.java
@@ -0,0 +1,37 @@
+package com.example.android.quakereport;
+
+/**
+ * Created by acme on 04-04-2018.
+ */
+
+public class Earthquake {
+
+
+    private double mMagnitude;
+    private String mLocation;
+    private long mTimeInMilliseconds;
+    private String mUrl;
+
+    public Earthquake(double magnitude, String location, long timeInMilliseconds, String url){
+        mMagnitude = magnitude;
+        mLocation = location;
+        mTimeInMilliseconds = timeInMilliseconds;
+        mUrl = url;
+    }
+
+    public double getMagnitude() {
+        return mMagnitude;
+    }
+
+    public String getLocation() {
+        return mLocation;
+    }
+
+    public long getTimeInMilliseconds() {
+        return mTimeInMilliseconds;
+    }
+
+    public String getUrl() {
+        return mUrl;
+    }
+}
